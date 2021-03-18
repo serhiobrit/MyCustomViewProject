e82c99257a3e0498cea0d8e4f7b9969730495502
Serhio Brit <itsserhioball@MacBook-Pro-Serhio.local>
2021-03-18 19:01:41 +0300

    initial commit

---
+++ MyCustomViewProject/MyCustomViewProject/MyCustomView.swift
@@ -0,0 +1,52 @@
+//
+//  MyCustomView.swift
+//  MyCustomViewProject
+//
+//  Created by Serhio Brit on 18.03.2021.
+//
+
+import UIKit
+
+class MyCustomView: UIView {
+    
+    @IBOutlet weak var labelForView: UILabel!
+    
+    var labelText: String {
+        get {
+            return labelForView.text!
+        }
+        set(labelText) {
+            labelForView.text = labelText
+        }
+    }
+    
+    var workingView: UIView!
+    var xibName: String = "MyCustomView"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setCustomView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setCustomView()
+    }
+    
+    func getFromXib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let xib = UINib(nibName: xibName, bundle: bundle)
+        let view = xib.instantiate(withOwner: self, options: nil).first as! UIView
+        
+        return view
+    }
+    
+    func setCustomView() {
+        workingView = getFromXib()
+        workingView.frame = bounds
+        workingView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        addSubview(workingView)
+    }
+    
+}
---
+++ MyCustomViewProject/MyCustomViewProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCustomViewProject
+//
+//  Created by Serhio Brit on 18.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
